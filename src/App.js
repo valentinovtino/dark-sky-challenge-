476dd61257283c7114663a6a1b9a7f5e23f8b9bf
Valentino Valentin <valentinovalentine95@gmail.com>
2018-10-18 23:43:19 -0600

    Render current weather

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import api from './helper/api.js';
 import Search from './Search.js';
+import CurrentWeather from './CurrentWeather.js'
 
  export default class App extends Component {
    constructor() {
@@ -32,15 +33,27 @@ import Search from './Search.js';
     return (
 
       // this.state.forecastObj ?
+     <div>
+
+
       <div className="app-background">
         <h1> DARK SKYES </h1>
         <Search getWeatherApi={this.getWeatherApi}/>
       </div>
 
-      // :
-      // <div>
-      //   <p> ERROR </p>
-      // </div>
+      <div className='current'>
+        <CurrentWeather 
+        latitude={this.state.forecastObj.latitude}
+        lagitude={this.state.forecastObj.longitude}
+        timezone={this.state.forecastObj.timezone}
+        time={this.state.forecastObj.currently.time}
+        summary={this.state.forecastObj.currently.summary}
+        temperature={this.state.forecastObj.currently.temperature}
+        />
+      </div>
+
+      
+      </div>
     );
   }
 }
